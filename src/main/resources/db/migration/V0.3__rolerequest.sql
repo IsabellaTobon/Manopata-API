99db97dd63f4a5e5eee3b8042ab16f76593636ba
Isabella T. <isabellatobon@gmail.com>
2024-07-16 10:48:04 +0200

    Add status each Endpoint

--- src/main/resources/db/migration/V0.3__rolerequest.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists roleendpoint (
-	id varchar(255) primary key not null,
-	role varchar(20) not null,
-	endpoint varchar(100) not null,
-	status boolean not null default true
-);
-
